18105457ddb9c75c06443d6adb9d696299c89612
Atul Verma <vatu;31@.com>
2026-06-15 12:14:01 +0530

    Recursion end now

--- l006NQUEENBITS.java
+++ l006NQUEENBITS.java
@@ -7,7 +7,9 @@ public class l006NQUEENBITS {
         cols = new boolean[n];
         digs = new boolean[n + n - 1];
         antidigs = new boolean[n + n - 1];
-        System.out.println(nqueen_01bits(n, 4, 0, ""));
+        // System.out.println(nqueen_01bits(n, 4, 0, ""));
+
+        System.out.println(nqueen_03bits(n, 0, ""));
 
     }
 
@@ -32,6 +34,7 @@ public class l006NQUEENBITS {
         return cnt;
     }
 
+    // NQUEEN using the Bits solution
     static int row, col, dig, antidig = 0;
 
     public static int nqueen_01bits(int n, int tnq, int bno, String psf) {
@@ -62,4 +65,56 @@ public class l006NQUEENBITS {
         }
         return cnt;
     }
+    // NQUEEN Most optimized solution using the Bits
+
+    public static int nqueen_03bits(int n, int floor, String psf) {
+        if (floor == n) {
+            System.out.println(psf);
+            return 1;
+        }
+        int cnt = 0;
+        int r = floor;
+        for (int c = 0; c < n; c++) { // loop on colunm
+            if ((row & (1 << r)) == 0 && (col & (1 << c)) == 0 && (dig & (1 << r - c + n - 1)) == 0
+                    && (antidig & (1 << r + c)) == 0) {
+
+                row ^= 1 << r;
+                col ^= 1 << c;
+                dig ^= (1 << r - c + n - 1);
+                antidig ^= (1 << r + c);
+                cnt += nqueen_03bits(n, floor + 1, psf + "(" + r + "," + c + ") ");
+                row ^= 1 << r;
+                col ^= 1 << c;
+                dig ^= (1 << r - c + n - 1);
+                antidig ^= (1 << r + c);
+
+            }
+
+        }
+        return cnt;
+    }
+
+    // all palindromic partition pepcoding
+    public static boolean isPalindrome(String str, int i, int j) {
+        while (i <= j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void solution(String str, int i, String asf) {
+        if (i >= str.length()) {
+            System.out.println(asf);
+            return;
+        }
+        for (int j = i; j < str.length(); j++) {
+            if (isPalindrome(str, i, j)) {
+                solution(str, j + 1, asf + "(" + str.substring(i, j + 1) + ") ");
+            }
+        }
+    }
+
 }
