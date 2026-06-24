a19d8b729badbf03d3739d97dce1f7a0b89f4987
Atul Verma <vatu;31@.com>
2026-06-24 20:44:34 +0530

    String Set done

--- DP/l002_String_Leetcode.java
+++ DP/l002_String_Leetcode.java
@@ -185,6 +185,146 @@ public class l002_String_Leetcode {
         }
     }
 
+    // tabulation
+    public static int countWays_tabu(String s1, String s2, int N, int M, int[][] dp) {
+        // code here
+        for (int n = 0; n <= N; n++) {
+            for (int m = 0; m <= M; m++) {
+                if (m == 0) {
+                    dp[n][m] = 1;
+                    continue;
+                } else if (n == 0) {
+                    dp[n][m] = 0;
+                    continue;
+                }
+                if (s1.charAt(n - 1) == s2.charAt(m - 1)) {
+                    dp[n][m] = dp[n - 1][m - 1] + dp[n - 1][m];
+                } else {
+                    dp[n][m] = dp[n - 1][m];
+                }
+            }
+        }
+        return dp[N][M];
+    }
+
+    // ======================================================LeetCode
+    // 583=============================================
+
+    // same the copy code of Longest comman subsequence and in main we calculate the
+    // answer
+    // length(s1)+length(s2) - 2 * (length of Longest comman subsequence)
+    // codes are same as
+    public int minDistance_Maincaller(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int[] d : dp) {
+            Arrays.fill(d, -1);
+        }
+        return n + m - 2 * longestCommonSubsequence_memo(s1, s2, n, m, dp);
+    }
+
+    public static int minDistance583_memo(String s1, String s2, int n, int m, int[][] dp) {
+        if (n == 0 || m == 0) {
+            return dp[n][m] = 0;
+        }
+
+        if (dp[n][m] != -1)
+            return dp[n][m];
+        // character match
+        int a = longestCommonSubsequence_memo(s1, s2, n - 1, m - 1, dp);
+        // not match then reduce from str1
+        int b = longestCommonSubsequence_memo(s1, s2, n - 1, m, dp);
+        // not match then reduce from str2
+        int c = longestCommonSubsequence_memo(s1, s2, n, m - 1, dp);
+        if (s1.charAt(n - 1) == s2.charAt(m - 1)) {
+            return dp[n][m] = a + 1;
+        } else {
+            return dp[n][m] = Math.max(b, c);
+        }
+
+    }
+
+    // ===============================LEETCODE
+    // 1035=============================================
+    // aggain same as Longest comman subsequence
+    // Dp solution
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int N = nums1.length;
+        int M = nums2.length;
+        int[][] dp = new int[N + 1][M + 1];
+        for (int n = 0; n <= N; n++) {
+            for (int m = 0; m <= M; m++) {
+                if (n == 0 || m == 0) {
+                    dp[n][m] = 0;
+                    continue;
+
+                }
+                int a = dp[n - 1][m - 1];
+                int b = dp[n - 1][m];
+                int c = dp[n][m - 1];
+                if (nums1[n - 1] == nums2[m - 1]) {
+                    dp[n][m] = a + 1;
+                } else {
+                    dp[n][m] = Math.max(b, c);
+                }
+            }
+        }
+        return dp[N][M];
+    }
+
+    // =============================================Leetcode 5
+    // ========================================
+    // tabulation
+    public String getLongestPal(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        int len = 0;
+        int startidx = 0;
+        int count = 0;
+        for (int gap = 0; gap < n; gap++) {
+            for (int i = 0, j = gap; j < n; j++, i++) {
+                if (gap == 0) {
+                    dp[i][j] = 1;
+                } else if (gap == 1 && s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = 2;
+                } else {
+                    dp[i][j] = s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1] > 0 ? dp[i + 1][j - 1] + 2 : 0;
+                }
+                if (len < dp[i][j]) {
+                    len = dp[i][j];
+                    startidx = i;
+                }
+                // total palindrom
+                count += dp[i][j] > 0 ? 1 : 0;
+
+            }
+        }
+        return s.substring(startidx, startidx + len);
+
+    }
+
+    // =======================================================gfg=========================================
+    // link:
+    // https://www.geeksforgeeks.org/problems/count-subsequences-of-type-ai-bj-ck4425/1
+    // code
+    public int fun(String s) {
+        // Write your code here
+        int m = 1000000007;
+        int acnt = 0, bcnt = 0, ccnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == 'a') {
+                acnt = (1 + 2 * acnt % m) % m; // we can write this is long form==>(acnt%m+(1+acnt%m)%m)%m;
+            } else if (ch == 'b') {
+                bcnt = (acnt % m + 2 * bcnt % m) % m; // we can write this is long form==>(bcnt%m+(acnt%m+bcnt%m)%m)%m;
+            } else if (ch == 'c') {
+                ccnt = (bcnt % m + 2 * ccnt % m) % m; // we can write this is long form==>(ccnt%m+(bcnt%m+ccnt%m))%m;
+            }
+        }
+        return (ccnt % m);
+    }
+
     public static void main(String[] args) {
         String s1 = "babgbag";
         String s2 = "bag";
@@ -195,7 +335,7 @@ public class l002_String_Leetcode {
         for (int[] d : dp) {
             Arrays.fill(d, -1);
         }
-        System.out.println(countWays_memo(s1, s2, n, m, dp));
+        System.out.println(countWays_tabu(s1, s2, n, m, dp));
         display2D(dp);
     }
 
