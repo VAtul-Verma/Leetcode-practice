d233710b4d88c24c66012d6cad5f0ba7bc5a3d33
Atul Verma <vatu;31@.com>
2026-07-29 20:26:45 +0530

    Array and String

--- Array_String/l001.java
+++ Array_String/l001.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class l001 {
 
     // ===================================LeetCode
@@ -282,15 +285,588 @@ public class l001 {
         return maxwater;
     }
 
+    // ====================================LEETCODE 3
+    // ===================================
+    // Naive Approach : this approach will only work with Charactrer
+    // Tc:O(n^2)
+    // GFG working code not on LeetCode
+    public int longestUniqueSubstr(String s) {
+        // code here
+        int maxLen = Integer.MIN_VALUE;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            boolean[] vis = new boolean[26];
+            for (int j = i; j < n; j++) {
+                if (vis[s.charAt(j) - 'a'] == true)
+                    break;
+                else {
+                    maxLen = Math.max(maxLen, j - i + 1);
+                    vis[s.charAt(j) - 'a'] = true;
+                }
+            }
+        }
+        return maxLen;
+
+    }
+
+    // =better approach ==using windows sliding techmique=================
+    // TC: o(n)
+    public int lengthOfLongestSubstring(String s) {
+        // code here
+        int si = 0;
+        int ei = 0;
+        int n = s.length();
+        int len = 0;
+        int cnt = 0;
+        int freq[] = new int[128];
+        while (ei < n) {
+            char ch = s.charAt(ei);
+            if (freq[ch] > 0) {
+                cnt++;
+            }
+            freq[ch]++;
+            ei++;
+
+            while (cnt > 0) {
+                char startchar = s.charAt(si);
+                if (freq[startchar] > 1) {
+                    cnt--;
+                }
+                freq[startchar]--;
+                si++;
+
+            }
+            len = Math.max(len, ei - si);
+        }
+        return len;
+    }
+
+    // ================================Leetcode
+    // 159=======================================
+    // Naive Appraoch :
+    // TC:O(n^1) Sp always 2 size hashmap so can say the constaint space;
+    public static int longSubstring(String s) {
+        int n = s.length();
+        int maxLen = 0;
+        int gsi = 0;
+        int gei = 0;
+        ;
+        for (int i = 0; i < n; i++) {
+            HashMap<Character, Integer> map = new HashMap<>();
+            for (int j = i; j < n; j++) {
+                char ch = s.charAt(j);
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+                if (map.size() > 2) {
+                    break;
+                }
+                int len = j - i + 1;
+                if (len > maxLen) {
+                    maxLen = len;
+                    gsi = i;
+                    gei = j;
+                }
+                maxLen = Math.max(maxLen, j - i + 1);
+            }
+
+        }
+        System.out.println(s.substring(gsi, gei + 1));
+        return maxLen;
+    }
+
+    // better approach :
+    // TC: O(n) : using the windows sliding technique
+    public static int longSubstring_better(String s) {
+        int freq[] = new int[128];
+        int n = s.length();
+        int si = 0;
+        int ei = 0;
+        int cnt = 0;
+        int maxLen = 0;
+        while (ei < n) {
+            char ch = s.charAt(ei);
+            if (freq[ch] == 0) {
+                cnt++;
+            }
+            freq[ch]++;
+            ei++;
+
+            while (cnt > 2) {
+                char removechar = s.charAt(si);
+                freq[removechar]--;
+                si++;
+                if (freq[removechar] == 0)
+                    cnt--;
+            }
+            maxLen = Math.max(maxLen, ei - si);
+        }
+        return maxLen;
+
+    }
+
+    // using HashMap
+    public static int longSubstring_Map(String s) {
+        int n = s.length();
+        Map<Character, Integer> count = new HashMap<>();
+        int i = 0, maxLen = 0;
+
+        for (int j = 0; j < n; j++) {
+            count.put(s.charAt(j), count.getOrDefault(s.charAt(j), 0) + 1);
+
+            // If more than 2 distinct characters, shrink from left
+            while (count.size() > 2) {
+                char c = s.charAt(i);
+                count.put(c, count.get(c) - 1);
+                if (count.get(c) == 0)
+                    count.remove(c);
+                i++;
+            }
+
+            // Update maximum length
+            maxLen = Math.max(maxLen, j - i + 1);
+        }
+
+        return maxLen;
+    }
+
+    // ===========================================Leetcode 76
+    // =================================================
+    // brute froces: check that string str2 all charater are present in
+    // the str1's each substring and return the minimum len substring
+
+    // TC: O(n^3) S.C:O(1)
+    public static boolean hasAllcharacter(String str, String t) {
+        int[] freq = new int[256];
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            freq[ch]++;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            freq[ch]--;
+        }
+
+        for (int i = 0; i < 256; i++) {
+            if (freq[i] > 0)
+                return false;
+        }
+        return true;
+    }
+
+    public String minWindow(String s, String t) {
+        String res = "";
+        int minLen = (int) 1e9;
+        int ns = s.length();
+        int nt = t.length();
+        for (int i = 0; i < ns; i++) {
+            for (int j = i; j < ns; j++) {
+                String substr = s.substring(i, j + 1);
+                if (hasAllcharacter(substr, t)) {
+                    int currlen = substr.length();
+                    if (currlen < minLen) {
+                        minLen = currlen;
+                        res = substr;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    // Better Approach:
+    public String minWindow_better(String s, String t) {
+        int ns = s.length();
+        int nt = t.length();
+        int[] freq = new int[128];
+        for (int i = 0; i < nt; i++) {
+            char ch = t.charAt(i);
+            freq[ch]++;
+        }
+        int si = 0;
+        int ei = 0;
+        int count = nt;
+        int len = (int) 1e9;
+        int gsi = 0;
+        while (ei < ns) {
+            char ch = s.charAt(ei);
+            if (freq[ch] > 0) {
+                count--;
+            }
+            freq[ch]--;
+            ei++;
+            while (count == 0) {
+                if (ei - si < len) {
+                    len = ei - si;
+                    gsi = si;
+                }
+                char removechar = s.charAt(si);
+                if (freq[removechar] == 0) {
+                    count++;
+                }
+                freq[removechar]++;
+                si++;
+
+            }
+        }
+        return len == (int) 1e9 ? "" : s.substring(gsi, gsi + len);
+    }
+
+    // ===============================Smallest distinct
+    // window==============================
+    // Link: https://www.geeksforgeeks.org/problems/smallest-distant-window3132/1
+
+    // Navie Approach: TC: O(n^2);
+
+    public int findSubString(String str) {
+
+        boolean vis[] = new boolean[256];
+        int n = str.length();
+        for (int i = 0; i < n; i++) {
+            char ch = str.charAt(i);
+            if (!vis[ch]) {
+                vis[ch] = true;
+            }
+        }
+        int minlen = (int) 1e9;
+        for (int i = 0; i < n; i++) {
+            boolean[] curr = new boolean[256];
+            for (int j = i; j < n; j++) {
+                char ch = str.charAt(j);
+                curr[ch] = true;
+                if (hascharacter(curr, vis)) {
+                    minlen = Math.min(minlen, j - i + 1);
+                }
+            }
+        }
+        return minlen;
+
+    }
+
+    public boolean hascharacter(boolean[] curr, boolean[] vis) {
+        int n = curr.length;
+
+        for (int i = 0; i < n; i++) {
+            if (vis[i] != curr[i])
+                return false;
+        }
+        return true;
+    }
+
+    // Better Approach
+    // TC: O(2n)
+    public int findSubString_better(String str) {
+        int[] freq = new int[256];
+        int n = str.length();
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            char ch = str.charAt(i);
+            if (freq[ch] == 0) {
+                freq[ch] = 1;
+                cnt++;
+            }
+        }
+        int si = 0, ei = 0, len = (int) 1e9;
+        int actualLen = cnt;
+        while (ei < n) {
+            char ch = str.charAt(ei);
+            if (freq[ch] > 0) {
+                cnt--;
+            }
+            freq[ch]--;
+            ei++;
+            while (cnt == 0) {
+                if (ei - si < len) {
+                    len = ei - si;
+                }
+                char dropchar = str.charAt(si);
+                if (freq[dropchar] == 0)
+                    cnt++;
+                freq[dropchar]++;
+                si++;
+            }
+            if (len == actualLen)
+                break;
+        }
+        return len;
+    }
+
+    // ==============================leetcode
+    // 340===========================================
+    public static int lengthoglongestSubtringWithkDistinctCharacter(String s, int k) {
+        int n = s.length();
+        int si = 0;
+        int ei = 0;
+        int cnt = 0;
+        int len = (int) -1e9;
+        int freq[] = new int[128];
+        while (ei < n) {
+            char ch = s.charAt(ei);
+            if (freq[ch] == 0) {
+                cnt++;
+
+            }
+            freq[ch]++;
+            ei++;
+            while (cnt > k) {
+                char remvoechar = s.charAt(si);
+                if (freq[ch] == 1) {
+                    cnt--;
+                }
+                freq[ch]--;
+                si++;
+            }
+            if (len < ei - si) {
+                len = ei - si;
+            }
+        }
+        return len;
+    }
+
+    // ====================================Leetcode
+    // 1456========================================
+    // Naive Approach TC: O(n^k)
+    public int maxVowels(String s, int k) {
+        int Maxvowel = 0;
+        int n = s.length();
+        if (n < k)
+            return n;
+        for (int i = 0; i <= n - k; i++) {
+            int vowelcnt = 0;
+            for (int j = i; j < i + k; j++) {
+                char ch = s.charAt(j);
+                if (ch == 'a' || ch == 'i' || ch == 'e' || ch == 'o' || ch == 'u') {
+                    vowelcnt++;
+                }
+            }
+            Maxvowel = Math.max(Maxvowel, vowelcnt);
+        }
+        return Maxvowel;
+    }
+
+    // better Approach :using the window sliding techinge
+
+    // =========================================GFG Longest Substring with K
+    // Uniques==================
+    // Link:
+    // https://www.geeksforgeeks.org/problems/longest-k-unique-characters-substring0853/1
+
+    // Naive Approach: O(n^2)
+    public int longestKSubstr(String s, int k) {
+        // code here
+        int n = s.length();
+        int maxLen = -1;
+        for (int i = 0; i < n; i++) {
+            int[] freq = new int[128];
+            int uniquecnt = 0;
+            for (int j = i; j < n; j++) {
+
+                char ch = s.charAt(j);
+                if (freq[ch] == 0)
+                    uniquecnt++;
+
+                freq[ch]++;
+
+                if (uniquecnt == k)
+                    maxLen = Math.max(maxLen, j - i + 1);
+                else if (uniquecnt > k)
+                    break;
+            }
+
+        }
+        return maxLen;
+
+    }
+
+    // Better Approach: TC : O(N) using the windows sliding
+    public int longestKSubstr_better(String s, int k) {
+        // code here
+        int n = s.length();
+        int si = 0, ei = 0, cnt = 0, maxLen = -1;
+        int freq[] = new int[128];
+
+        while (ei < n) {
+            char ch = s.charAt(ei);
+            if (freq[ch] == 0) {
+                cnt++;
+            }
+            freq[ch]++;
+            ei++;
+            while (cnt > k) {
+                char removechar = s.charAt(si);
+                if (freq[removechar] == 1) {
+                    cnt--;
+                }
+                freq[removechar]--;
+                si++;
+
+            }
+            if (cnt == k) {
+                maxLen = Math.max(maxLen, ei - si);
+            }
+        }
+        return maxLen;
+
+    }
+
+    // ===============================================LEETOCDE
+    // 992=======================================
+
+    // ==========================================LEETCODE
+    // 1248==============================
+    // naive appraoch: TC: O(n^2);
+    public int numberOfSubarrays(int[] arr, int k) {
+        int ans = 0;
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (int j = i; j < n; j++) {
+                if (arr[j] % 2 == 1)
+                    cnt++;
+                if (cnt == k) {
+                    ans++;
+
+                }
+            }
+        }
+        return ans;
+    }
+
+    // better appraoch TC : O(N)
+    // find the atmost k and atmost k -1 then get the exact by atmost(k)-atmost(k-1)
+    public int numberOfSubarrays_better(int[] nums, int k) {
+        return AtmostKOddArray(nums, k) - AtmostKOddArray(nums, k - 1);
+    }
+
+    public int AtmostKOddArray(int[] arr, int k) {
+        int n = arr.length;
+        int si = 0;
+        int ei = 0;
+        int cnt = 0;
+        int ans = 0;
+        while (ei < n) {
+            if (arr[ei] % 2 == 1)
+                cnt++;
+            ei++;
+            while (cnt > k) {
+                if (arr[si] % 2 == 1)
+                    cnt--;
+                si++;
+            }
+            ans += ei - si;
+        }
+        return ans;
+    }
+
+    // =============================LEETCODE 904=================================
+    // naive approach TC: O(N)
+    public int totalFruit(int[] arr) {
+        HashMap<Integer, Integer> m = new HashMap<>();
+        int si = 0, ei = 0, ans = 0, n = arr.length;
+        while (ei < n) {
+            m.put(arr[ei], m.getOrDefault(arr[ei], 0) + 1);
+
+            ei++;
+            while (m.size() > 2) {
+                m.put(arr[si], m.getOrDefault(arr[si], 0) - 1);
+                if (m.get(arr[si]) == 0)
+                    m.remove(arr[si]);
+                si++;
+            }
+            ans = Math.max(ans, ei - si);
+        }
+        return ans;
+    }
+
+    // ==================================LEETCODE 930========================
+    // Naive Appraoch : O(N^2)
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int n = nums.length;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += nums[j];
+                if (sum == goal)
+                    ans++;
+            }
+        }
+        return ans;
+    }
+
+    // Better aprraoch
+    // TC: O(N)
+    public int numSubarraysWithSum_better(int[] arr, int k) {
+        return atmostK_Helper(arr, k) - (k != 0 ? atmostK_Helper(arr, k - 1) : 0);
+    }
+
+    public int atmostK_Helper(int[] arr, int k) {
+
+        int n = arr.length, si = 0, ei = 0, sum = 0, count = 0;
+        while (ei < n) {
+            sum += arr[ei++];
+
+            while (sum > k) {
+                sum -= arr[si++];
+            }
+            count += ei - si;
+
+        }
+        return count;
+
+    }
+
+    // ==============================LeetCode 485================================
+    // Naive Approach O(n^2)
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int n = nums.length;
+        int maxlen = 0;
+        for (int i = 0; i < n; i++) {
+            int len = 0;
+            for (int j = i; j < n; j++) {
+                if (nums[j] == 0)
+                    break;
+                else {
+                    len++;
+                }
+            }
+            maxlen = Math.max(len, maxlen);
+        }
+        return maxlen;
+
+    }
+
+    // better Approach
+    // TC: O(n);
+    public int findMaxConsecutiveOnes_(int[] nums) {
+        int n = nums.length;
+        int si = 0;
+        int ei = 0, len = 0, maxLen = 0;
+        while (ei < n) {
+            if (nums[ei] == 1)
+                ei++;
+            else {
+                len = ei - si;
+                maxLen = Math.max(maxLen, len);
+                si = ei + 1;
+                ei++;
+            }
+        }
+        // Check the last sequence of 1s
+        maxLen = Math.max(maxLen, ei - si);
+        return maxLen;
+    }
+
     public static void main(String args[]) {
         // int[] arr = { -12, 11, -13, -5, 6, -7, 5, -3, -6 };
         // Rearrange_withOrder(arr);
 
         // int arr[] = { 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0 };
         // int arr[] = { 1, 1, 1, 1, 1 };
-        int arr[] = { 2, 0, 1, 1, 0, 1, 2 };
-        sortColors(arr);
-        print1D(arr);
+        // int arr[] = { 2, 0, 1, 1, 0, 1, 2 };
+        // sortColors(arr);
+        // print1D(arr);
+
+        System.out.println(longSubstring_better("geeksforgeeks"));
 
     }
 
