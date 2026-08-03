56dd98c299e3470b1cd271dd6c48a458f280792a
Atul Verma <vatu;31@.com>
2026-08-03 23:20:26 +0530

    LinkedList

--- LinkedList/l001.java
+++ LinkedList/l001.java
@@ -1,5 +1,6 @@
 import java.lang.classfile.components.ClassPrinter.ListNode;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class l001 {
 
@@ -109,6 +110,180 @@ public class l001 {
             j--;
         }
         return true;
+    }
+
+    // Better Approach O(N)
+    public boolean isPalindrome_better(ListNode head) {
+        ListNode midhead = middleoftheList(head);
+        ListNode newhead = midhead.next;
+        midhead.next = null;
+        boolean res = true;
+        newhead = reverseList_helper(newhead);
+        ListNode c1 = head;
+        ListNode c2 = newhead;
+        while (c1 != null && c2 != null) {
+            if (c1.val != c2.val) {
+                res = false;
+                break;
+            }
+            ;
+            c1 = c1.next;
+            c2 = c2.next;
+        }
+
+        // make the Linked List as Given in starting
+        newhead = reverseList_helper(newhead);
+        midhead.next = newhead;
+        ListNode curr = head;
+        // print the list
+        // while (curr != null) {
+        // System.out.print(curr.val);
+        // curr = curr.next;
+        // }
+        return res;
+    }
+
+    // this will give the second mid
+    public ListNode middleoftheList(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    public ListNode reverseList_helper(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode fast = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = fast;
+        }
+        return prev;
+    }
+
+    // ==============================================Leetcode
+    // 143==================================
+    // Naive appraoch TC:O(N) SC:O(N)
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null)
+            return;
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        // Store all values
+        ListNode curr = head;
+        while (curr != null) {
+            arr.add(curr.val);
+            curr = curr.next;
+        }
+
+        // Create reordered values
+        ArrayList<Integer> ans = new ArrayList<>();
+        int i = 0, j = arr.size() - 1;
+
+        while (i < j) {
+            ans.add(arr.get(i));
+            ans.add(arr.get(j));
+            i++;
+            j--;
+        }
+
+        if (i == j) {
+            ans.add(arr.get(i));
+        }
+
+        // Copy values back into the original list
+        curr = head;
+        int idx = 0;
+        while (curr != null) {
+            curr.val = ans.get(idx++);
+            curr = curr.next;
+        }
+    }
+
+    // ====Better Approach ===========================
+    public ListNode middleoftheList02(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    public ListNode reverseList_helper02(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode fast = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = fast;
+        }
+        return prev;
+    }
+
+    public void reorderList_better(ListNode head) {
+        if (head == null)
+            return;
+        ListNode mid = middleoftheList02(head);
+        ListNode newhead = mid.next;
+        mid.next = null;
+        newhead = reverseList_helper02(newhead);
+        ListNode c1 = head;
+        ListNode c2 = newhead;
+        while (c2 != null) {
+            ListNode f1 = c1.next;
+            ListNode f2 = c2.next;
+            c1.next = c2;
+            c2.next = f1;
+            c1 = f1;
+            c2 = f2;
+
+        }
+
+    }
+
+    // ==================================LEETCODE 21 ===========================
+    // Naive approach using arraylist
+    public ListNode mergeTwoLists(ListNode head1, ListNode head2) {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        while (head1 != null) {
+            arr.add(head1.val);
+            head1 = head1.next;
+
+        }
+        while (head2 != null) {
+            arr.add(head2.val);
+            head2 = head2.next;
+
+        }
+        Collections.sort(arr);
+
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        for (int i = 0; i < arr.size(); i++) {
+            prev.next = new ListNode(arr.get(i));
+            prev = prev.next;
+        }
+
+        return dummy.next;
+
     }
 
     public static void main(String[] args) {
